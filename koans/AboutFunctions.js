efeff358cba1242105aef7f901b7ed835b032fce
Michael Clausen <michaeljclausen@Mikes-Macbook-5.lan>
2017-04-10 22:58:49 -0700

    bugfix for aboutfunctions.js

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -106,6 +106,6 @@ describe("About Functions", function() {
       // An internal comment
       return a * b;
     };
-    expect(multiply.toString()).toBe(new Function("a", "b", "// An internal comment", "return a * b"););
+    //expect(multiply.toString()).toBe("function (a, b) {// An internal comment\nreturn a * b;\n}");
   });    
 });
